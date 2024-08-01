06ff2599e9810a86ca01b2117c34fb2327d7ee76
Aditya <lapratomo24@yahoo.com>
2024-08-01 22:02:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+## Guess the Number ##
+
+logo = '''
+  _____                   __  __         _  __           __          
+ / ___/_ _____ ___ ___   / /_/ /  ___   / |/ /_ ____ _  / /  ___ ____
+/ (_ / // / -_|_-<(_-<  / __/ _ \/ -_) /    / // /  ' \/ _ \/ -_) __/
+\___/\_,_/\__/___/___/  \__/_//_/\__/ /_/|_/\_,_/_/_/_/_.__/\__/_/   
+'''
+
+print("")
+print("Welcome to Guess the Number game!")
+print("We pick a number for you to guess. What's your difficulty?")
+difficulty = input("Choose between 'beginner' and 'advanced': ")
+
